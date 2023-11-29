3be0b16ac2658ea4840b5f27463b287b9920dbd5
Nick Crews <nicholas.b.crews@gmail.com>
2023-11-28 15:07:23 -0900

    attempt using libpostal
    This doesn't quite seem to link.
    It manages to compile.
    I must not be using cmake
    and specifiying the search paths quite right.

--- src/libpostal_extension.cpp
+++ src/libpostal_extension.cpp
@@ -8,6 +8,8 @@
 #include "duckdb/main/extension_util.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
+#include <libpostal/libpostal.h>
+
 namespace duckdb {
 
 inline void LibpostalScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
@@ -19,9 +21,37 @@ inline void LibpostalScalarFun(DataChunk &args, ExpressionState &state, Vector &
         });
 }
 
+inline void ParseAddress(DataChunk &args, ExpressionState &state, Vector &result) {
+    libpostal_address_parser_options_t options = libpostal_get_address_parser_default_options();
+    auto &text_vector = args.data[0];
+    UnaryExecutor::Execute<string_t, string_t>(
+	    text_vector, result, args.size(),
+	    [&](string_t name) {
+            libpostal_address_parser_response_t *parsed = libpostal_parse_address((char *)"781 Franklin Ave Crown Heights Brooklyn NYC NY 11216 USA", options);
+			auto ret = StringVector::AddString(result, parsed->components[0]);;
+            libpostal_address_parser_response_destroy(parsed);
+            return ret;
+        });
+
+
+
+
+}
+
 static void LoadInternal(DatabaseInstance &instance) {
+    if (!libpostal_setup() || !libpostal_setup_parser()) {
+        exit(EXIT_FAILURE);
+    }
+
     auto libpostal_scalar_function = ScalarFunction("libpostal", {LogicalType::VARCHAR}, LogicalType::VARCHAR, LibpostalScalarFun);
     ExtensionUtil::RegisterFunction(instance, libpostal_scalar_function);
+
+    auto libpostal_parse_address = ScalarFunction("libpostal_parse_address", {LogicalType::VARCHAR}, LogicalType::VARCHAR, ParseAddress);
+    ExtensionUtil::RegisterFunction(instance, libpostal_parse_address);
+
+    // Teardown (only called once at the end of your program)
+    // libpostal_teardown();
+    // libpostal_teardown_parser();
 }
 
 void LibpostalExtension::Load(DuckDB &db) {
